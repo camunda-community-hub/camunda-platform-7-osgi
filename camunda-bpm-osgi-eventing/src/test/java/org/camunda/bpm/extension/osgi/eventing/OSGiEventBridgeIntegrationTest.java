a14dde88f6e1c7fa83a4c6cad99102f63d8758dc
Ronny Bräunlich <r.braeunlich@gmail.com>
2015-03-05 20:13:43 +0100

    chore: update versions and adjust tests master branch

--- camunda-bpm-osgi-eventing/src/test/java/org/camunda/bpm/extension/osgi/eventing/OSGiEventBridgeIntegrationTest.java
+++ camunda-bpm-osgi-eventing/src/test/java/org/camunda/bpm/extension/osgi/eventing/OSGiEventBridgeIntegrationTest.java
@@ -27,7 +27,6 @@ import javax.inject.Inject;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.Dictionary;
 import java.util.Hashtable;
@@ -44,103 +43,74 @@ import static org.ops4j.pax.exam.CoreOptions.*;
 @ExamReactorStrategy(PerClass.class)
 public class OSGiEventBridgeIntegrationTest {
 
-		@Inject
-		private BundleContext bundleContext;
+  @Inject
+  private BundleContext bundleContext;
 
-		@Inject
-		private EventAdmin eventAdmin;
+  @Inject
+  private EventAdmin eventAdmin;
 
-		@Inject
-		@Filter(timeout = 30000L)
-		private OSGiEventBridgeActivator eventBridgeActivator;
+  @Inject
+  @Filter(timeout = 30000L)
+  private OSGiEventBridgeActivator eventBridgeActivator;
 
-		@Configuration
-		public Option[] createConfiguration() {
-				Option[] camundaBundles = options(
-								mavenBundle().groupId("org.camunda.bpm")
-												.artifactId("camunda-engine").version("7.2.0"),
-								mavenBundle().groupId("org.camunda.bpm.model")
-												.artifactId("camunda-xml-model")
-												.version("7.2.0"),
-								mavenBundle().groupId("org.camunda.bpm.model")
-												.artifactId("camunda-bpmn-model")
-												.version("7.2.0"),
-								mavenBundle().groupId("org.camunda.bpm.model")
-												.artifactId("camunda-cmmn-model")
-												.version("7.2.0"),
-								mavenBundle().groupId("org.camunda.commons")
-												.artifactId("camunda-commons-logging")
-												.version("1.0.5-SNAPSHOT"),
-								mavenBundle().groupId("org.camunda.commons")
-												.artifactId("camunda-commons-utils")
-												.version("1.0.5-SNAPSHOT"),
-								mavenBundle().groupId("org.camunda.bpm.extension.osgi")
-												.artifactId("camunda-bpm-osgi")
-												.version("1.1.0-SNAPSHOT"),
-								mavenBundle().groupId("org.camunda.bpm.extension.osgi")
-												.artifactId("camunda-bpm-osgi-eventing-api")
-												.version("1.1.0-SNAPSHOT"),
-								mavenBundle().groupId("joda-time").artifactId("joda-time")
-												.version("2.1"),
-								mavenBundle().groupId("com.h2database").artifactId("h2")
-												.version("1.2.143"),
-								// FIXME this Mybatis version doesn't match camunda's
-								mavenBundle().groupId("org.mybatis").artifactId("mybatis")
-												.version("3.2.7"),
-								mavenBundle().groupId("org.apache.logging.log4j")
-												.artifactId("log4j-api").version("2.0-beta9"),
-								mavenBundle().groupId("org.apache.logging.log4j")
-												.artifactId("log4j-core").version("2.0-beta9")
-												.noStart(),
-								mavenBundle().groupId("org.slf4j").artifactId("slf4j-api")
-												.version("1.7.7"),
-								mavenBundle().groupId("ch.qos.logback")
-												.artifactId("logback-core").version("1.1.2"),
-								mavenBundle().groupId("ch.qos.logback")
-												.artifactId("logback-classic").version("1.1.2"),
-								mavenBundle().groupId("org.assertj")
-												.artifactId("assertj-core").version("1.5.0"),
-								mavenBundle().groupId("org.apache.felix")
-												.artifactId("org.apache.felix.eventadmin")
-												.version("1.4.2"),
-								mavenBundle().groupId("org.apache.felix").artifactId("org.apache.felix.dependencymanager")
-												.version("3.2.0"),
-								// make sure compiled classes from src/main are included
-								bundle("reference:file:target/classes"));
-				return OptionUtils.combine(camundaBundles, CoreOptions.junitBundles());
-		}
+  @Configuration
+  public Option[] createConfiguration() {
+    Option[] camundaBundles = options(
+      mavenBundle("org.camunda.bpm", "camunda-engine").versionAsInProject(),
+      mavenBundle("org.camunda.bpm.model", "camunda-bpmn-model").versionAsInProject(),
+      mavenBundle("org.camunda.bpm.model", "camunda-cmmn-model").versionAsInProject(),
+      mavenBundle("org.camunda.bpm.model", "camunda-xml-model").versionAsInProject(),
 
-		@Test
-		public void shouldRegisterService() {
-				assertThat(eventBridgeActivator, is(notNullValue()));
-		}
+      mavenBundle("joda-time", "joda-time").versionAsInProject(),
+      mavenBundle("com.h2database", "h2").versionAsInProject(),
+      mavenBundle("org.mybatis", "mybatis").versionAsInProject(),
+      mavenBundle("com.fasterxml.uuid", "java-uuid-generator").versionAsInProject(),
 
-		@Test
-		public void testEventBrigde() throws FileNotFoundException {
-				TestEventHandler eventHandler = new TestEventHandler();
-				registerEventHandler(eventHandler);
-				ProcessEngine processEngine = createProcessEngine();
-				DeploymentBuilder deploymentBuilder = processEngine.getRepositoryService().createDeployment();
-				deploymentBuilder.name("testProcess").addInputStream("testProcess.bpmn", new FileInputStream(new File(
-								"src/test/resources/testProcess.bpmn"))).deploy();
-				processEngine.getRuntimeService().startProcessInstanceByKey("Process_1");
-				assertThat(eventHandler.isCalled(), is(true));
-		}
 
-		private ProcessEngine createProcessEngine() {
-				StandaloneInMemProcessEngineConfiguration configuration = new StandaloneInMemProcessEngineConfiguration();
-				configuration.setCustomPreBPMNParseListeners(Collections.<BpmnParseListener>singletonList(eventBridgeActivator));
-				ProcessEngineFactoryWithELResolver engineFactory = new ProcessEngineFactoryWithELResolver();
-				engineFactory.setProcessEngineConfiguration(configuration);
-				engineFactory.setBundle(bundleContext.getBundle());
-				engineFactory.setExpressionManager(new OSGiExpressionManager());
-				engineFactory.init();
-				return engineFactory.getObject();
-		}
+      mavenBundle("org.camunda.bpm.extension.osgi", "camunda-bpm-osgi")
+        .versionAsInProject(),
+      mavenBundle("org.camunda.bpm.extension.osgi", "camunda-bpm-osgi-eventing-api")
+        .versionAsInProject(),
+      mavenBundle("org.apache.felix", "org.apache.felix.eventadmin")
+        .versionAsInProject(),
+      mavenBundle("org.apache.felix", "org.apache.felix.dependencymanager")
+        .versionAsInProject(),
+      // make sure compiled classes from src/main are included
+      bundle("reference:file:target/classes"));
+    return OptionUtils.combine(camundaBundles, CoreOptions.junitBundles());
+  }
 
-		private void registerEventHandler(TestEventHandler eventHandler) {
-				Dictionary props = new Hashtable();
-				props.put(EventConstants.EVENT_TOPIC, Execution.class.getName().replace('.', '/'));
-				bundleContext.registerService(EventHandler.class.getName(), eventHandler, props);
-		}
+  @Test
+  public void shouldRegisterService() {
+    assertThat(eventBridgeActivator, is(notNullValue()));
+  }
+
+  @Test
+  public void testEventBrigde() throws FileNotFoundException {
+    TestEventHandler eventHandler = new TestEventHandler();
+    registerEventHandler(eventHandler);
+    ProcessEngine processEngine = createProcessEngine();
+    DeploymentBuilder deploymentBuilder = processEngine.getRepositoryService().createDeployment();
+    deploymentBuilder.name("testProcess").addInputStream("testProcess.bpmn", new FileInputStream(new File(
+      "src/test/resources/testProcess.bpmn"))).deploy();
+    processEngine.getRuntimeService().startProcessInstanceByKey("Process_1");
+    assertThat(eventHandler.isCalled(), is(true));
+  }
+
+  private ProcessEngine createProcessEngine() {
+    StandaloneInMemProcessEngineConfiguration configuration = new StandaloneInMemProcessEngineConfiguration();
+    configuration.setCustomPreBPMNParseListeners(Collections.<BpmnParseListener>singletonList(eventBridgeActivator));
+    ProcessEngineFactoryWithELResolver engineFactory = new ProcessEngineFactoryWithELResolver();
+    engineFactory.setProcessEngineConfiguration(configuration);
+    engineFactory.setBundle(bundleContext.getBundle());
+    engineFactory.setExpressionManager(new OSGiExpressionManager());
+    engineFactory.init();
+    return engineFactory.getObject();
+  }
+
+  private void registerEventHandler(TestEventHandler eventHandler) {
+    Dictionary props = new Hashtable();
+    props.put(EventConstants.EVENT_TOPIC, Execution.class.getName().replace('.', '/'));
+    bundleContext.registerService(EventHandler.class.getName(), eventHandler, props);
+  }
 }
--- camunda-bpm-osgi-eventing/src/test/java/org/camunda/bpm/extension/osgi/eventing/TestEventHandler.java
+++ camunda-bpm-osgi-eventing/src/test/java/org/camunda/bpm/extension/osgi/eventing/TestEventHandler.java
@@ -1,21 +1,26 @@
 package org.camunda.bpm.extension.osgi.eventing;
 
+import org.camunda.bpm.extension.osgi.eventing.api.BusinessProcessEventProperties;
 import org.osgi.service.event.Event;
 import org.osgi.service.event.EventHandler;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
 
 /**
  * @author Ronny Bräunlich
  */
 public class TestEventHandler implements EventHandler {
 
-		private boolean called = false;
+  private boolean called = false;
 
-		@Override
-		public void handleEvent(Event event) {
-			this.called = true;
-		}
+  @Override
+  public void handleEvent(Event event) {
+    this.called = true;
+    assertThat((String) event.getProperty(BusinessProcessEventProperties.PROCESS_DEFINITION), is("Process_1"));
+  }
 
-		public boolean isCalled(){
-				return called;
-		}
+  public boolean isCalled() {
+    return called;
+  }
 }
