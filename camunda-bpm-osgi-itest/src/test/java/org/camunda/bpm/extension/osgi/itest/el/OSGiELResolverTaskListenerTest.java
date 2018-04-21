43be880923313604168c552ae30cf1fcdf4b6707
avh <avh@cedros.com>
2018-04-21 23:33:18 +0200

    test for calling a tenant specific subprocess. The tenant id is dynamically calculated by a CalledElementTenantIdProvider. It is resolved through an delegate expression in the business process model.

--- camunda-bpm-osgi-itest/src/test/java/org/camunda/bpm/extension/osgi/itest/el/OSGiELResolverTaskListenerTest.java
+++ camunda-bpm-osgi-itest/src/test/java/org/camunda/bpm/extension/osgi/itest/el/OSGiELResolverTaskListenerTest.java
@@ -1,10 +1,6 @@
 package org.camunda.bpm.extension.osgi.itest.el;
 
-import org.camunda.bpm.engine.delegate.JavaDelegate;
 import org.camunda.bpm.engine.delegate.TaskListener;
-import org.camunda.bpm.engine.runtime.ProcessInstance;
-import org.hamcrest.CoreMatchers;
-import org.hamcrest.Matcher;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.ops4j.pax.exam.junit.PaxExam;
@@ -12,24 +8,23 @@ import org.ops4j.pax.exam.spi.reactors.ExamReactorStrategy;
 import org.ops4j.pax.exam.spi.reactors.PerMethod;
 
 import java.io.File;
-import java.util.Hashtable;
 
-import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 @RunWith(PaxExam.class)
 @ExamReactorStrategy(PerMethod.class)
-public class OSGIELResolverTaskListenerTest extends AbstractOSGiELResolverIntegrationTest{
+public class OSGiELResolverTaskListenerTest extends AbstractOSGiELResolverIntegrationTest {
   @Override
   protected File getProcessDefinition() {
     return new File("src/test/resources/el/testTaskListener.bpmn");
   }
 
   @Test
-  public void runProcess() throws Exception {
+  public void runProcess() {
     JustAnotherTaskListener service = new JustAnotherTaskListener();
     ctx.registerService(TaskListener.class, service, null);
-    ProcessInstance processInstance = processEngine.getRuntimeService()
+    processEngine.getRuntimeService()
       .startProcessInstanceByKey("taskListenerTestProcess");
     assertThat(service.called, is(true));
   }
